58f38521867714d3621211c5ef7cbf6a92c7ccb7
Uroš Jarc <uros.jarc@ixtlan-team.si>
2023-12-16 04:27:09 +0100

    Ading log versioning.

--- src/main/kotlin/com/urosjarc/dbanalyser/app/App.kt
+++ src/main/kotlin/com/urosjarc/dbanalyser/app/App.kt
@@ -7,25 +7,26 @@ import com.urosjarc.dbanalyser.app.logs.LogService
 import com.urosjarc.dbanalyser.app.schema.SchemaRepo
 import com.urosjarc.dbanalyser.app.table.TableRepo
 import com.urosjarc.dbanalyser.app.table.TableService
+import kotlinx.datetime.Clock
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 
 object App {
-    fun modul() = module {
-        this.single<DbRepo> { DbRepo("db.json") }
-        this.single<LogRepo> { LogRepo("log.json") }
-        this.single<ClientRepo> { ClientRepo() }
-        this.single<TableRepo> { TableRepo() }
-        this.single<SchemaRepo> { SchemaRepo() }
+	fun modul() = module {
+		this.single<DbRepo> { DbRepo("db.json") }
+		this.single<LogRepo> { LogRepo("log_${Clock.System.now()}.json") }
+		this.single<ClientRepo> { ClientRepo() }
+		this.single<TableRepo> { TableRepo() }
+		this.single<SchemaRepo> { SchemaRepo() }
 
-        this.single { TableService(get()) }
-        this.single { LogService(get()) }
-    }
+		this.single { TableService(get()) }
+		this.single { LogService(get()) }
+	}
 
-    fun init() {
-        startKoin { this.modules(modul()) }
-    }
+	fun init() {
+		startKoin { this.modules(modul()) }
+	}
 
-    fun reset() = stopKoin()
+	fun reset() = stopKoin()
 }
